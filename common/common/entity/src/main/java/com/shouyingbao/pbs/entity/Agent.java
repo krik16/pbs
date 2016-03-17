5f44ef3c252738f22067240d57323811c8832700
kejun <kejun@rongyi.com>
2016-03-17 16:32:27 +0800

    area

--- common/common/entity/src/main/java/com/shouyingbao/pbs/entity/Agent.java
+++ common/common/entity/src/main/java/com/shouyingbao/pbs/entity/Agent.java
@@ -1,35 +1,14 @@
 package com.shouyingbao.pbs.entity;
 
 import java.io.Serializable;
-import java.util.Date;
 
-public class Agent implements Serializable {
+public class Agent extends BaseEntity implements Serializable {
     private static final long serialVersionUID = 1875419709216731429L;
-    private Integer id;
-
     private String name;
 
     private String desc;
 
     private Integer areaId;
-
-    private Date createAt;
-
-    private Integer createBy;
-
-    private Date updateAt;
-
-    private Integer updateBy;
-
-    private Byte isDelete;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -55,43 +34,13 @@ public class Agent implements Serializable {
         this.areaId = areaId;
     }
 
-    public Date getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
-    }
-
-    public Integer getCreateBy() {
-        return createBy;
-    }
-
-    public void setCreateBy(Integer createBy) {
-        this.createBy = createBy;
-    }
-
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public Integer getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(Integer updateBy) {
-        this.updateBy = updateBy;
-    }
-
-    public Byte getIsDelete() {
-        return isDelete;
-    }
-
-    public void setIsDelete(Byte isDelete) {
-        this.isDelete = isDelete;
+    @Override
+    public String toString() {
+        return "Agent{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                ", areaId=" + areaId +
+                ", BaseEntity=" + super.toString() +
+                '}';
     }
 }
